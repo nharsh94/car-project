ff6cc6b921928277c8f5399f1b07735b5d8c7464
noahD <noahharshb@gmail.com>
2024-02-06 15:03:12 -0700

    salespeople get and put working

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AutomobileVO(models.Model):
-    import_href = models.CharField(max_length=300, unique=True)
+    import_href = models.CharField(max_length=300, unique=True, blank=True)
     vin = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField()
     color = models.CharField(max_length=50)
@@ -15,7 +15,7 @@ class AutomobileVO(models.Model):
 class Salesperson(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
-    employee_id = models.CharField(max_length=25)
+    employee_id = models.CharField(max_length=25, unique=True, blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'

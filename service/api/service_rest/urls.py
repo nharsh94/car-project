dd57df4f15a1a56db5f62e81f5660978608763af
Tim <timmccormack88@gmail.com>
2024-02-07 09:51:58 -0800

    new file:   service/api/service_rest/migrations/0001_initial.py 	modified:   service/api/service_rest/urls.py

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -4,7 +4,7 @@ from .views import technicians, delete_technician, appointments, delete_appointm
 
 urlpatterns = [
     path('service/', technicians, name="technicians"),
-    path('service/<int:id>/', delete_technician, name="delete_technician"),
+    path('service/<int:pk>/', delete_technician, name="delete_technician"),
     path('service/', appointments, name="appointments"),
-    path('service/<int:id>/', delete_appointment, name="delete_appointment")
+    path('service/<int:pk>/', delete_appointment, name="delete_appointment")
 ]

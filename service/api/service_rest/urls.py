f5210770d88f86fd1035e66d51b35d3a60709806
Tim <timmccormack88@gmail.com>
2024-02-08 10:15:15 -0800

    modified:   service/api/service_rest/urls.py 	modified:   service/api/service_rest/views.py

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import technicians, delete_technician, appointments, delete_appointment, cancel_appointment, finish_appointment
+from .views import technicians, delete_technician, appointments, delete_appointment
 
 urlpatterns = [
     path('service/technicians/', technicians, name="technicians"),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('service/delete/appointment/<int:pk>/', delete_appointment, name="delete_appointment"),
 
     path('service/appointments/', appointments, name="appointments"),
-    path('service/cancel/appointment/<int:pk>/', cancel_appointment, name="cancel_appointment"),
-    path('service/finish/appointment/<int:pk>/', finish_appointment, name="finish_appointment")
+    # path('service/cancel/appointment/<int:pk>/', cancel_appointment, name="cancel_appointment"),
+    # path('service/finish/appointment/<int:pk>/', finish_appointment, name="finish_appointment")
 
 ]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_http_methods
 from .models import Technician, Appointment
 import json
 from common.json import ModelEncoder
-from django.http import HttpResponse
+
 
 class AppointmentDetailEncoder(ModelEncoder):
     model = Appointment
@@ -44,26 +44,7 @@ def delete_technician(request, pk=None):
 
 @require_http_methods(["GET", "POST"])
 def appointments(request):
-    if request.method == "GET":
-        appointments = Appointment.objects.all()
-        return JsonResponse(
-        {"appointments": appointments},
-            encoder=AppointmentDetailEncoder,
-        )
-    else:
-        try:
-            content = json.loads(request.body)
-            appointment = Appointment.objects.create(**content)
-            return JsonResponse(
-                appointment,
-                encoder=AppointmentDetailEncoder,
-                safe=False)
-        except:
-            response = JsonResponse(
-                {"message": "Could not create an appointment"}
-            )
-            response.status_code = 400
-            return response
+
 
 @require_http_methods(["DELETE"])
 def delete_appointment(request, pk=None):
@@ -71,45 +52,45 @@ def delete_appointment(request, pk=None):
         deleted, _ = Appointment.objects.filter(id=pk).delete()
         return JsonResponse({"deleted" : deleted > 0})
 
-@require_http_methods(["PUT"])
-def cancel_appointment(request, pk=None):
-    if request.method == "PUT":
-        content = json.loads(request.body)
-        try:
-            appointment.objects.filter(id=pk).update(**content)
-            appointment = Appointment.objects.get(id=pk)
-            appointment.status = "canceled"
-            return HttpResponse(status=200)
-        except Appointment.DoesNotExist:
-            response = JsonResponse(
-                {"message": f"Appointment with id {pk} does not exist"}
-            )
-            response.status_code = 404
-            return response
-        except Exception as e:
-            response = JsonResponse(
-                {"message": f"Error canceling appointment: {str(e)}"}
-            )
-            response.status_code = 500
-            return response
+# @require_http_methods(["PUT"])
+# def cancel_appointment(request, pk=None):
+#     if request.method == "PUT":
+#         content = json.loads(request.body)
+#         try:
+#             appointment.objects.filter(id=pk).update(**content)
+#             appointment = Appointment.objects.get(id=pk)
+#             appointment.status = "canceled"
+#             return HttpResponse(status=200)
+#         except Appointment.DoesNotExist:
+#             response = JsonResponse(
+#                 {"message": f"Appointment with id {pk} does not exist"}
+#             )
+#             response.status_code = 404
+#             return response
+#         except Exception as e:
+#             response = JsonResponse(
+#                 {"message": f"Error canceling appointment: {str(e)}"}
+#             )
+#             response.status_code = 500
+#             return response
 
-@require_http_methods(["PUT"])
-def finish_appointment(request, pk=None):
-    if request.method == "PUT":
-        try:
-            appointment = Appointment.objects.get(id=pk)
-            appointment.status = "finished"
-            appointment.save()
-            return HttpResponse(status=200)
-        except Appointment.DoesNotExist:
-            response = JsonResponse(
-                {"message": f"Appointment with id {pk} does not exist"}
-            )
-            response.status_code = 404
-            return response
-        except Exception as e:
-            response = JsonResponse(
-                {"message": f"Error finishing appointment: {str(e)}"}
-            )
-            response.status_code = 500
-            return response
+# @require_http_methods(["PUT"])
+# def finish_appointment(request, pk=None):
+#     if request.method == "PUT":
+#         try:
+#             appointment = Appointment.objects.get(id=pk)
+#             appointment.status = "finished"
+#             appointment.save()
+#             return HttpResponse(status=200)
+#         except Appointment.DoesNotExist:
+#             response = JsonResponse(
+#                 {"message": f"Appointment with id {pk} does not exist"}
+#             )
+#             response.status_code = 404
+#             return response
+#         except Exception as e:
+#             response = JsonResponse(
+#                 {"message": f"Error finishing appointment: {str(e)}"}
+#             )
+#             response.status_code = 500
+#             return response

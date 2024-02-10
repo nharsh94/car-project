aca5a51c9e0e2718e1f01f56fa98c050cfba5d81
Noah Harshbarger <noahharshb@gmail.com>
2024-02-09 17:46:43 -0700

    SaleForm missing value for Auto handle change

--- ghi/app/src/SaleForm.js
+++ ghi/app/src/SaleForm.js
@@ -85,6 +85,7 @@ function SaleForm() {
     }
     const handleAutomobileChange = (event) => {
         const value = event.target.value;
+        setAutomobile(value);
     }
 
     const handleSalespersonChange = (event) => {
@@ -107,7 +108,7 @@ function SaleForm() {
                 <form onSubmit={handleSubmit} id="create-sale-form">
                     <div className="mb-3">
                         <select value={automobile} onChange={handleAutomobileChange} required name="automobile" id="automobile" className="form-select">
-                            <option value="">Choose an automobile VIN</option>
+                            <option value="">Choose a VIN</option>
                             {autos.map(automobile => {
                                 return (
                                     <option key={automobile.vin} value={automobile.id}>
@@ -119,7 +120,7 @@ function SaleForm() {
                     </div>
                     <div className="mb-3">
                             <select value={salesperson} onChange={handleSalespersonChange} required name="salesperson" id="salesperson" className="form-select">
-                                <option value="">Choose a salesperson</option>
+                                <option value="">Choose a Salesperson</option>
                                 {salespeople.map(salesperson => {
                                     return (
                                         <option key={salesperson.employee_id} value={salesperson.employee_id}>
@@ -131,7 +132,7 @@ function SaleForm() {
                     </div>
                     <div>
                         <select value={customer} onChange={handleCustomerChange} required name="customer" id="customer" className="form-select">
-                            <option value="">Choose a customer</option>
+                            <option value="">Choose a Customer</option>
                             {customers.map(customer => {
                                 return (
                                     <option key={customer.id} value={customer.id}>
